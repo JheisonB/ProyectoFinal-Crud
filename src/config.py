053b57b7c3374763c38a5f8eda1873dfce773d17
E1PO11O <yeuriv6@outlook.com>
2022-06-17 21:40:53 -0400

    .

--- src/config.py
+++ src/config.py
@@ -1,5 +1,15 @@
-class DevelopmentConfig():
+from flask import Config
+
+
+class config:
+    SECRET_KEY = 'B!1weNAT1T^%kvhUI*S^'
+
+class DevelopmentConfig(Config):
     DEBUG=True
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = '123456'
+    MYSQL_DB = 'flask_login'
 
 config={
     'development':DevelopmentConfig
