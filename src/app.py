022e7384aeaeb7907a29806e56783e133a753887
E1PO11O <yeuriv6@outlook.com>
2022-06-17 22:07:26 -0400

    Previo a la conexion con una base de datos

--- src/app.py
+++ src/app.py
@@ -1,9 +1,11 @@
-
 from flask import Flask, render_template, request, redirect, url_for
+from flask_mysqldb import MySQL
 
 from config import config
 
-app=Flask(__name__)
+app = Flask(__name__)
+
+db = MySQL(app) 
 
 @app.route('/')
 def index():
